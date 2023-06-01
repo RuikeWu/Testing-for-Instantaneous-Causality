276e37e220806fde7ec38c67e2bbb94551e14e1d
RuikeWu <37581160+RuikeWu@users.noreply.github.com>
2023-06-01 23:08:37 +0800

    Add files via upload

--- Test_inst_causal_by_npbs_cv.m
+++ Test_inst_causal_by_npbs_cv.m
@@ -1,30 +1,33 @@
-% Y is d X T input data, d is the dimension, T is sample size
-% p is lag length of vector autoregressive model
-% B is bootstrap replication 
-% d_1 is the dimension of u1
+% Y id input data with 2 X T, T is sample size
+% p is the number of lag term
+% B is the loop for bootstrap procedure 
+% d_1 is the dimension for first part
 % cf is confidence level such as 0.95
-% h0 is bandwidth coefficient, the bandwidth applied is h =  h0*T^(-0.2);
 
-function [pval,J_T,ifa] = Test_inst_causal_by_npbs(Y,p,B,d_1,cf,h0)
+function [pval,J_T,ifa] = Test_inst_causal_by_npbs_cv(Y,p,B,d_1,cf)
     [d,T] = size(Y);
-    h = h0*T^(-0.2);
-    
+
     % Compute OLS estimator
-    Id = eye(d);
-    md = varm(d,p);
-    md.Constant = [0 ; 0];
-    A = estimate(md,Y');
-    K_part1 = [];
-    for k_loop =  1 : p
-        K_part1 = [K_part1,A.AR{k_loop}];
-    end
-    II_hat_matlab = K_part1;
-    II_hat = reshape(II_hat_matlab,[size(II_hat_matlab,1)*size(II_hat_matlab,2) 1]);
-   
-    % Obtain residual u_hat
+    Id = eye(d);   
+    V0 = zeros(p*d,p*d);
+    V3 = zeros(d,p*d);
+    for t = (p+1):T
+        X_t_1 = [];
+        for i = 1 : p
+            X_t_1 = [X_t_1,(Y(:,t-i))'];
+        end
+        X_t_1 = X_t_1';
+        V0 = V0 + X_t_1* X_t_1';
+        V3 = V3 + Y(:,t) * X_t_1';
+    end 
+    V1 = kron(V0,Id);
+    V4 = reshape(V3,[d*p*d,1]);
+    II_hat = inv(V1)*V4; 
+    
+    %  obtain residual u_hat
     u_hat = [];
     X_t_1_set = [];
-    for t = p+1:T 
+    for t = p+1:T %从p+1开始循环，p是lag数
          X_t_1 = [];
         for i = 1 : p
             X_t_1 = [X_t_1,(Y(:,t-i))'];
@@ -32,6 +35,13 @@ function [pval,J_T,ifa] = Test_inst_causal_by_npbs(Y,p,B,d_1,cf,h0)
         X_t_1 = X_t_1';
         X_t_1_set(:,t-p) = X_t_1;
         u_hat(:,t-p) = Y(:,t)-(kron(X_t_1',Id))*II_hat; 
+    end
+    % calculate u1*u2
+    cov_x_set = [];
+    for x = 1 : size(u_hat,2)
+        u1_hat = u_hat(1:d_1,x);
+        u2_hat = u_hat(d_1+1:d,x);
+        cov_x_set(x) = u1_hat * u2_hat';
     end
     
     %define u1 and u2 with d1 d2 , d1 + d2 = d
@@ -42,6 +52,7 @@ function [pval,J_T,ifa] = Test_inst_causal_by_npbs(Y,p,B,d_1,cf,h0)
     for ii = 1: B     
         m_hat_i = []; 
         yibusu_set = [];
+        cov_i_set = []; 
         for i = 1: size(u_hat,2)
             u1i_hat = u_hat(1:d_1,i);
             u2i_hat = u_hat(d_1+1:d,i);
@@ -49,8 +60,13 @@ function [pval,J_T,ifa] = Test_inst_causal_by_npbs(Y,p,B,d_1,cf,h0)
             mi_hat = reshape(cov_i,[d_1*d_2,1]);
             yibusu_it = randn(1);
             m_hat_i(:,i) = yibusu_it * mi_hat;
-         end
-    
+            cov_i_set(i) = yibusu_it * cov_i;
+        end
+        
+        % using cross-validation to select bandwidth
+        h = cv_for_band_bt(cov_i_set,T);
+        
+        
         % obtain lambda_hat phi_hat and J_T
         lambda_hat = 0;
         phi_hat = 0;
@@ -71,6 +87,7 @@ function [pval,J_T,ifa] = Test_inst_causal_by_npbs(Y,p,B,d_1,cf,h0)
     
     
     %Calculate J_T
+    h = cv_for_band_bt(cov_x_set,T);    
     m_hat = [];
     d_2 = d -d_1; % dimension of u2
     for i = 1: size(u_hat,2)
@@ -80,7 +97,8 @@ function [pval,J_T,ifa] = Test_inst_causal_by_npbs(Y,p,B,d_1,cf,h0)
         mi_hat = reshape(cov_i,[d_1*d_2,1]);
         m_hat(:,i) = mi_hat;
     end
-    
+
+    % obtain lambda_hat phi_hat and J_T
     lambda_hat = 0;
     phi_hat = 0;
     for t = 1 : size(m_hat,2)
---
+++ cv_for_band_bt.m
@@ -0,0 +1,26 @@
+function [h_opt,c_opt,j_opt] = cv_for_band_bt(cov_x_set,T)
+    
+    j_set = 1:30; 
+    c_set = 1.05.^(j_set-15); 
+    h_set = (T^(-1/5)).*c_set; % set the range of candidate bandwidth
+    CV_set = [];
+    for jj = 1 : length(h_set)
+        h0 = h_set(jj);
+        tau_x_set = [];
+        s_set = 1:T;
+        for  x = 2 :T
+            ker_set = kernel(T,x,s_set,h0);
+            ker_set(x) = 0; % leave-one-out estimator         
+            ker_set(1) =[];
+            hat_tau_x_set = ker_set.*cov_x_set;
+            hat_tau_x = sum(hat_tau_x_set)/sum(ker_set);
+            tau_x_set(x-1)= hat_tau_x;
+        end  
+        CV_set(jj) =  sum((cov_x_set - tau_x_set).^2); 
+    end
+    [~,loc] = min(CV_set);
+    h_opt = h_set(loc);
+    j_opt = j_set(loc);
+    c_opt = c_set(loc);
+
+end
